a0e1609db14729c9dab0e3c625043800cf2c85bd
Tumeyy <stefan.tumey@gmail.com>
2022-09-10 15:20:04 -0400

    absolute and relative positioning fun

--- App.js
+++ App.js
@@ -13,14 +13,16 @@ export default function App() {
     }}>
       <View style={{
         backgroundColor: 'dodgerblue',
-        width: 400,
-        flex: -1,
+        width: 100,
         height: 100,
       }}/>
       <View style={{
         backgroundColor: 'gold',
         width: 100,
-        height: 100
+        height: 100,
+        top: 20,
+        left: 20,
+        position: 'absolute'
       }}/>
       <View style={{
         backgroundColor: 'tomato',
