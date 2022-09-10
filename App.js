7d365ffc6b9ea4e3cb94e72a3456b61d52ad017f
Tumeyy <stefan.tumey@gmail.com>
2022-09-09 21:55:51 -0400

    Fun with touchable events

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
-      <Image
-        blurRadius={10}
-        source={{
-          width: 200,
-          height: 300,
-          uri : "https://picsum.photos/200/300"}}/>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri : "https://picsum.photos/200/300"}}/>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
